deea0520c362e818ed292be0d0e390fdf26dfe5d
Sascha Weidner <support@sioweb.de>
2017-07-17 07:26:04 +0200

    PHP 7 + Contao 4

--- dca/tl_news_archive.php
+++ dca/tl_news_archive.php
@@ -1,23 +1,23 @@
-<?php
-
-/**
- * Contao Open Source CMS
- */
-
-/**
- * @file tl_news_archive.php
- * @author Sascha Weidner
- * @version 3.0.0
- * @package sioweb.contao.extensions.glossar
- * @copyright Sascha Weidner, Sioweb
- */
-
-$Semikolon = substr($GLOBALS['TL_DCA']['tl_news_archive']['palettes']['default'],-1);
-$GLOBALS['TL_DCA']['tl_news_archive']['palettes']['default'] .= ($Semikolon===';'?'':';').'{glossar_legend},glossar_disallow';
-$GLOBALS['TL_DCA']['tl_news_archive']['fields']['glossar_disallow'] = array(
-	'label'                   => &$GLOBALS['TL_LANG']['tl_news_archive']['glossar_disallow'],
-	'exclude'                 => true,
-	'filter'                  => true,
-	'inputType'               => 'checkbox',
-	'sql'                     => "char(1) NOT NULL default ''"
+<?php
+
+/**
+ * Contao Open Source CMS
+ */
+
+/**
+ * @file tl_news_archive.php
+ * @author Sascha Weidner
+ * @version 3.0.0
+ * @package sioweb.contao.extensions.glossar
+ * @copyright Sascha Weidner, Sioweb
+ */
+
+$Semikolon = substr($GLOBALS['TL_DCA']['tl_news_archive']['palettes']['default'],-1);
+$GLOBALS['TL_DCA']['tl_news_archive']['palettes']['default'] .= ($Semikolon===';'?'':';').'{glossar_legend},glossar_disallow';
+$GLOBALS['TL_DCA']['tl_news_archive']['fields']['glossar_disallow'] = array(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_news_archive']['glossar_disallow'],
+	'exclude'                 => true,
+	'filter'                  => true,
+	'inputType'               => 'checkbox',
+	'sql'                     => "char(1) NOT NULL default ''"
 );
